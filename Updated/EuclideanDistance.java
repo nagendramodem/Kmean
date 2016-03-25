f18317b8d4aa31df97ea09483a1921b87468a87e
nagendramodem <@7N71a1226>
2016-03-25 10:59:49 -0400

    change

--- Updated/EuclideanDistance.java
+++ Updated/EuclideanDistance.java
@@ -1,25 +1,4 @@
-/**
- * This file is part of the Java Machine Learning Library
- * 
- * The Java Machine Learning Library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * The Java Machine Learning Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with the Java Machine Learning Library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * Copyright (c) 2006-2012, Thomas Abeel
- * 
- * Project: http://java-ml.sourceforge.net/
- * 
- */
+
 package net.sf.javaml.clustering;
 
 import net.sf.javaml.core.Instance;
@@ -38,7 +17,6 @@ import net.sf.javaml.core.Instance;
  * 
  * @linkplain http://en.wikipedia.org/wiki/Euclidean_distance
  * @linkplain http://en.wikipedia.org/wiki/Euclidean_space
- * @author Thomas Abeel
  * 
  */
 public class EuclideanDistance extends NormDistance {
